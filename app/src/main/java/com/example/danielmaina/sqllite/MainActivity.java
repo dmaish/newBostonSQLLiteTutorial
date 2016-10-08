bafc82779984ff2971db7d7e227fd6e7202ed489
dmaish <mainadaniel81@gmail.com>
2016-10-08 17:43:50 +0300

    "code to put and display items in the database complete but problem with the create-table query"

--- app/src/main/java/com/example/danielmaina/sqllite/MainActivity.java
+++ app/src/main/java/com/example/danielmaina/sqllite/MainActivity.java
@@ -2,18 +2,54 @@ package com.example.danielmaina.sqllite;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyDBHandler dbHandler;
+    TextView myTextView;
+    EditText myEditext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText myEditext = (EditText) findViewById(R.id.myEditText);
-        TextView myTextView = (TextView) findViewById(R.id.myTextView);
+        myEditext = (EditText) findViewById(R.id.myEditText);
+        myTextView = (TextView) findViewById(R.id.myTextView);
 
-       MyDBHandler dbHandler = new MyDBHandler(this, null,null, 1);
+        dbHandler = new MyDBHandler(this, null,null, 1);
+        printDatabase();
+    }
+
+
+
+
+
+    //onClick method to add a product to the database
+    public void addButtonClicked(){
+        Products product = new Products(myEditext.getText().toString());
+        dbHandler.addProduct(product);
+    }
+    //Delete button onClick method
+    public void deleteButtonClicked(View view){
+        String inputText = myTextView.getText().toString();
+        dbHandler.deleteProduct(inputText);
+        printDatabase();
+
+    }
+
+
+
+
+
+
+
+
+    public void printDatabase (){
+        String dbString = dbHandler.databaseToString();
+        myTextView.setText(dbString);
+        myEditext.setText("");
     }
 }
